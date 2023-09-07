562b29b47e32315ef3d657e3e4e16d5ad26f9a1a
mklabs <mklabs@users.noreply.github.com>
2023-09-07 15:09:35 +0200

    deploy: combo-graph/combo-graph@dffa884773f37c1576640cfff1b5fe63af84f243

---
+++ builds/1.3.6-5.3-WindowsEditor-dffa884/json.js
@@ -0,0 +1,267 @@
+const json = {
+    "devices": [
+        {
+            "deviceName": "DESKTOP-TAT57NG",
+            "instance": "DESKTOP-TAT57NG-8C11EFB644728AF19B8358B767C44F4B-80BB2B98467D4EE3D9F123BCBD8B7755",
+            "platform": "WindowsEditor",
+            "oSVersion": "Windows 11 (22H2) [10.0.22621.2215] ",
+            "model": "Default",
+            "gPU": "NVIDIA GeForce RTX 3080",
+            "cPUModel": "AMD Ryzen 9 7950X 16-Core Processor            ",
+            "rAMInGB": 64,
+            "renderMode": "SM5",
+            "rHI": "",
+            "appInstanceLog": ""
+        }
+    ],
+    "reportCreatedOn": "2023.09.07-15.09.29",
+    "succeeded": 9,
+    "succeededWithWarnings": 0,
+    "failed": 0,
+    "notRun": 0,
+    "inProcess": 0,
+    "totalDuration": 12.86044692993164,
+    "comparisonExported": false,
+    "comparisonExportDirectory": "",
+    "tests": [
+        {
+            "testDisplayName": "should have attributes initialized",
+            "fullTestPath": "ComboGraph.Basics.should have attributes initialized",
+            "state": "Success",
+            "deviceInstance": [
+                "DESKTOP-TAT57NG-8C11EFB644728AF19B8358B767C44F4B-80BB2B98467D4EE3D9F123BCBD8B7755"
+            ],
+            "duration": 0.02588529884815216,
+            "dateTime": "0001.01.01-00.00.00",
+            "entries": [],
+            "warnings": 0,
+            "errors": 0,
+            "artifacts": []
+        },
+        {
+            "testDisplayName": "should setup world and such",
+            "fullTestPath": "ComboGraph.Basics.should setup world and such",
+            "state": "Success",
+            "deviceInstance": [
+                "DESKTOP-TAT57NG-8C11EFB644728AF19B8358B767C44F4B-80BB2B98467D4EE3D9F123BCBD8B7755"
+            ],
+            "duration": 0.011866200715303421,
+            "dateTime": "0001.01.01-00.00.00",
+            "entries": [],
+            "warnings": 0,
+            "errors": 0,
+            "artifacts": []
+        },
+        {
+            "testDisplayName": "should have attributes initialized",
+            "fullTestPath": "ComboGraph.Feature Testing.should have attributes initialized",
+            "state": "Success",
+            "deviceInstance": [
+                "DESKTOP-TAT57NG-8C11EFB644728AF19B8358B767C44F4B-80BB2B98467D4EE3D9F123BCBD8B7755"
+            ],
+            "duration": 0.13004030287265778,
+            "dateTime": "0001.01.01-00.00.00",
+            "entries": [
+                {
+                    "event": {
+                        "type": "Info",
+                        "message": "LogSkeletalMesh: Waiting for skinned assets to be ready 0/1 (SK_Mannequin) ...",
+                        "context": "",
+                        "artifact": "00000000000000000000000000000000"
+                    },
+                    "filename": "",
+                    "lineNumber": -1,
+                    "timestamp": "2023.09.07-13.09.15"
+                }
+            ],
+            "warnings": 0,
+            "errors": 0,
+            "artifacts": []
+        },
+        {
+            "testDisplayName": "should setup world and such",
+            "fullTestPath": "ComboGraph.Feature Testing.should setup world and such",
+            "state": "Success",
+            "deviceInstance": [
+                "DESKTOP-TAT57NG-8C11EFB644728AF19B8358B767C44F4B-80BB2B98467D4EE3D9F123BCBD8B7755"
+            ],
+            "duration": 0.010706301778554916,
+            "dateTime": "0001.01.01-00.00.00",
+            "entries": [],
+            "warnings": 0,
+            "errors": 0,
+            "artifacts": []
+        },
+        {
+            "testDisplayName": "wtf",
+            "fullTestPath": "ComboGraph.Feature Testing.wtf",
+            "state": "Success",
+            "deviceInstance": [
+                "DESKTOP-TAT57NG-8C11EFB644728AF19B8358B767C44F4B-80BB2B98467D4EE3D9F123BCBD8B7755"
+            ],
+            "duration": 0.01911100000143051,
+            "dateTime": "0001.01.01-00.00.00",
+            "entries": [
+                {
+                    "event": {
+                        "type": "Info",
+                        "message": "Definitely wtf",
+                        "context": "",
+                        "artifact": "00000000000000000000000000000000"
+                    },
+                    "filename": "",
+                    "lineNumber": -1,
+                    "timestamp": "2023.09.07-13.09.15"
+                },
+                {
+                    "event": {
+                        "type": "Info",
+                        "message": "Spec done",
+                        "context": "",
+                        "artifact": "00000000000000000000000000000000"
+                    },
+                    "filename": "",
+                    "lineNumber": -1,
+                    "timestamp": "2023.09.07-13.09.15"
+                }
+            ],
+            "warnings": 0,
+            "errors": 0,
+            "artifacts": []
+        },
+        {
+            "testDisplayName": "ComboGraph should be created",
+            "fullTestPath": "ComboGraph.Nodes.ComboGraph Task and Nodes API.ComboGraph should be created",
+            "state": "Success",
+            "deviceInstance": [
+                "DESKTOP-TAT57NG-8C11EFB644728AF19B8358B767C44F4B-80BB2B98467D4EE3D9F123BCBD8B7755"
+            ],
+            "duration": 0.011667996644973755,
+            "dateTime": "0001.01.01-00.00.00",
+            "entries": [],
+            "warnings": 0,
+            "errors": 0,
+            "artifacts": []
+        },
+        {
+            "testDisplayName": "EntryNode is created",
+            "fullTestPath": "ComboGraph.Nodes.ComboGraph Task and Nodes API.ComboGraph.EntryNode is created",
+            "state": "Success",
+            "deviceInstance": [
+                "DESKTOP-TAT57NG-8C11EFB644728AF19B8358B767C44F4B-80BB2B98467D4EE3D9F123BCBD8B7755"
+            ],
+            "duration": 0.011723499745130539,
+            "dateTime": "0001.01.01-00.00.00",
+            "entries": [],
+            "warnings": 0,
+            "errors": 0,
+            "artifacts": []
+        },
+        {
+            "testDisplayName": "FirstNode is created",
+            "fullTestPath": "ComboGraph.Nodes.ComboGraph Task and Nodes API.ComboGraph.FirstNode is created",
+            "state": "Success",
+            "deviceInstance": [
+                "DESKTOP-TAT57NG-8C11EFB644728AF19B8358B767C44F4B-80BB2B98467D4EE3D9F123BCBD8B7755"
+            ],
+            "duration": 0.011376399546861649,
+            "dateTime": "0001.01.01-00.00.00",
+            "entries": [],
+            "warnings": 0,
+            "errors": 0,
+            "artifacts": []
+        },
+        {
+            "testDisplayName": "F_ComboGraph_Test",
+            "fullTestPath": "Project.Functional Tests.ComboGraphTests.FunctionalTests.Maps.F_ComboGraph_Map.F_ComboGraph_Test",
+            "state": "Success",
+            "deviceInstance": [
+                "DESKTOP-TAT57NG-8C11EFB644728AF19B8358B767C44F4B-80BB2B98467D4EE3D9F123BCBD8B7755"
+            ],
+            "duration": 12.628069877624512,
+            "dateTime": "0001.01.01-00.00.00",
+            "entries": [
+                {
+                    "event": {
+                        "type": "Info",
+                        "message": "F_ComboGraph_Test: Float assertion passed (Should have stamina decreased by 10  (0))",
+                        "context": "",
+                        "artifact": "00000000000000000000000000000000"
+                    },
+                    "filename": "",
+                    "lineNumber": -1,
+                    "timestamp": "2023.09.07-13.09.19"
+                },
+                {
+                    "event": {
+                        "type": "Info",
+                        "message": "F_ComboGraph_Test: Float assertion passed (Should have stamina decreased by 10  (1))",
+                        "context": "",
+                        "artifact": "00000000000000000000000000000000"
+                    },
+                    "filename": "",
+                    "lineNumber": -1,
+                    "timestamp": "2023.09.07-13.09.20"
+                },
+                {
+                    "event": {
+                        "type": "Info",
+                        "message": "F_ComboGraph_Test: Float assertion passed (Should have stamina decreased by 10  (2))",
+                        "context": "",
+                        "artifact": "00000000000000000000000000000000"
+                    },
+                    "filename": "",
+                    "lineNumber": -1,
+                    "timestamp": "2023.09.07-13.09.22"
+                },
+                {
+                    "event": {
+                        "type": "Info",
+                        "message": "F_ComboGraph_Test: Float assertion passed (Should have stamina decreased by 10  (3))",
+                        "context": "",
+                        "artifact": "00000000000000000000000000000000"
+                    },
+                    "filename": "",
+                    "lineNumber": -1,
+                    "timestamp": "2023.09.07-13.09.24"
+                },
+                {
+                    "event": {
+                        "type": "Info",
+                        "message": "F_ComboGraph_Test: Float assertion passed (Should have stamina decreased by 10  (4))",
+                        "context": "",
+                        "artifact": "00000000000000000000000000000000"
+                    },
+                    "filename": "",
+                    "lineNumber": -1,
+                    "timestamp": "2023.09.07-13.09.25"
+                },
+                {
+                    "event": {
+                        "type": "Info",
+                        "message": "F_ComboGraph_Test: Float assertion passed (Should have stamina decreased by 10  (5))",
+                        "context": "",
+                        "artifact": "00000000000000000000000000000000"
+                    },
+                    "filename": "",
+                    "lineNumber": -1,
+                    "timestamp": "2023.09.07-13.09.27"
+                },
+                {
+                    "event": {
+                        "type": "Info",
+                        "message": "F_ComboGraph_Test: Float assertion passed (Should have stamina decreased to 25 after 3 attacks)",
+                        "context": "",
+                        "artifact": "00000000000000000000000000000000"
+                    },
+                    "filename": "",
+                    "lineNumber": -1,
+                    "timestamp": "2023.09.07-13.09.28"
+                }
+            ],
+            "warnings": 0,
+            "errors": 0,
+            "artifacts": []
+        }
+    ]
+};
